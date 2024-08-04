aaae10c46a8030d3dc41591576a56c6e6038bc50
sejaly <93701556+sejaly@users.noreply.github.com>
2024-08-05 00:13:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Configure CORS
 app.use(cors({
-  origin: 'https://car-management-systems.onrender.com', // Your frontend URL
+  origin: '*', // Your frontend URL
   
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
